75c8569e776f4027aee748bb882c012bb7d90ae5
Alex Liauw <aliauw@santander.com.br>
2019-12-17 16:12:23 -0300

    initial commit

---
+++ mockyPyServer.py
@@ -0,0 +1,67 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+#GET simples
+@app.route('/',methods = ['GET'])
+def hello():
+    resp = """
+    {
+     "name": "%s",
+     "instructions": "%s",
+     "teste": "%s"
+    }
+    """
+    resp = resp % ('José da Silva', 'superior', 'ok')
+    return resp, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+#GET com path param
+@app.route('/ids/<id>',methods = ['GET'])
+def id(id):
+    resp = """
+    {
+     "id": "%s",
+     "name": "%s",
+     "detail": "%s"
+    }
+    """
+    resp = resp % (id, 'MaLu', 'color hair')
+    return resp, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+#GET com query param
+@app.route('/search',methods = ['GET'])
+def search():
+    user = request.args.get('user')
+    name = request.args.get('name')
+
+    resp = """
+    {
+     "id": "%s",
+     "user": "%s",
+     "name": "%s"
+    }
+    """
+    resp = resp % ('112233', user, name)
+    return resp, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+#POST simples
+@app.route('/newid',methods = ['POST'])
+def newid():
+
+        req = request.json        
+        resp = """
+        {
+        "id": "%s",
+        "name": "%s",
+        "detail":{
+                "job":"%s",
+                "hobby": "%s"
+            }
+        "message": "%s"
+        }
+        """
+        resp = resp % (req['id'], req['name'], req['detail']['job'], req['detail']['hobby'], 'Salvo com Sucesso!!!')
+        return resp, 201, {'Content-Type': 'application/json; charset=utf-8'}
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8080, debug = True)
